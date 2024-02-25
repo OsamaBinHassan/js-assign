1e3bc63019ba8fd703e9717250c14a1cf7392ed3
Osama Bin Hassan <usamahassan6701@gmail.com>
2024-02-25 20:14:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// chap 1 tasks
+
+alert("Error!please enter a valid password");
+alert("Welcome to js land!! \n happy coding");
+alert("Welcome to js land!!");
+alert(" happy coding");
+alert("Hello...I can run js through my browser console log");
+Swal.fire({
+    title: 'JavaScript',
+    text: 'We can do this also! if we do little bit research',
+    icon: 'info',
+    confirmButtonText: 'OK',
+  });
+
+//chap-2
+
+let username;
+let myName = "John Doe";
+let message;
+message = "Hello World";
+alert(message);
+let studentName = "Osama";
+let studentAge = 1204;
+let studentGender = "Male";
+let studentCountry = "Pakistan";
+
+alert("Name: " + studentName + "\nAge: " + studentAge + "\nGender: " + studentGender + "\nCountry: " + studentCountry);
+
+var design = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”';
+alert(design)
